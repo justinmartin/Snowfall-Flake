8b7fb92e6d3ba80a9e4b26299ac8eceb0e584673
Justin Martin <jus10mar10@gmail.com>
2023-09-13 20:56:38 -0500

    Import of nixos modules

---
+++ modules/nixos/apps/inkscape/default.nix
@@ -0,0 +1,15 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.frgd;
+let cfg = config.frgd.apps.inkscape;
+in
+{
+  options.frgd.apps.inkscape = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Inkscape.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ inkscape-with-extensions google-fonts ];
+  };
+}
---
+++ modules/nixos/home/default.nix
@@ -0,0 +1,36 @@
+{ options, config, pkgs, lib, inputs, ... }:
+
+with lib;
+with lib.frgd;
+let cfg = config.frgd.home;
+in
+{
+  # imports = with inputs; [
+  #   home-manager.nixosModules.home-manager
+  # ];
+
+  options.frgd.home = with types; {
+    file = mkOpt attrs { }
+      (mdDoc "A set of files to be managed by home-manager's `home.file`.");
+    configFile = mkOpt attrs { }
+      (mdDoc "A set of files to be managed by home-manager's `xdg.configFile`.");
+    extraOptions = mkOpt attrs { } "Options to pass directly to home-manager.";
+  };
+
+  config = {
+    frgd.home.extraOptions = {
+      home.stateVersion = config.system.stateVersion;
+      home.file = mkAliasDefinitions options.frgd.home.file;
+      xdg.enable = true;
+      xdg.configFile = mkAliasDefinitions options.frgd.home.configFile;
+    };
+
+    home-manager = {
+      useUserPackages = true;
+      useGlobalPkgs = true;
+
+      users.${config.frgd.user.name} =
+        mkAliasDefinitions options.frgd.home.extraOptions;
+    };
+  };
+}
