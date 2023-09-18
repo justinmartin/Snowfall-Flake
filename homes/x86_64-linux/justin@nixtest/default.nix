fc7548564e72be18688f2846e14e1804d3522f3c
Justin Martin <jus10mar10@gmail.com>
2023-09-17 20:22:54 -0500

    add nixtest

---
+++ homes/x86_64-linux/justin@nixtest/default.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
+
+with lib.frgd;
+{
+  frgd = {
+    user = {
+      enable = true;
+      name = config.snowfallorg.user.name;
+    };
+
+    cli-apps = {
+      zsh = enabled;
+      neovim = enabled;
+      home-manager = enabled;
+    };
+
+    tools = {
+      git = enabled;
+      direnv = enabled;
+    };
+  };
+}
