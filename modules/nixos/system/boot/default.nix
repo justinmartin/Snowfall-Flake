d175b44273e49e68e71a0a3234d90a3f3500332d
Justin Martin <jus10mar10@gmail.com>
2023-10-02 21:34:22 -0500

    update boot to grub

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -10,11 +10,14 @@ in
   };
 
   config = mkIf cfg.enable {
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.systemd-boot.configurationLimit = 5;
-    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
+    # boot.loader.systemd-boot.enable = true;
+    # boot.loader.systemd-boot.configurationLimit = 5;
+    # boot.loader.efi.canTouchEfiVariables = true;
 
-    # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
-    boot.loader.systemd-boot.editor = false;
+    # # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
+    # boot.loader.systemd-boot.editor = false;
   };
 }
