d16c124a1d379bda49d78b4a01c573d1db97c564
Justin Martin <jus10mar10@gmail.com>
2023-10-03 20:30:20 -0500

    update nixos test config

--- systems/x86_64-linux/nixos/default.nix
+++ systems/x86_64-linux/nixos/default.nix
@@ -10,57 +10,10 @@
   ];
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  # networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  # time.timeZone = "America/Chicago";
-
-  # # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-
-  # i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "en_US.UTF-8";
-  #   LC_IDENTIFICATION = "en_US.UTF-8";
-  #   LC_MEASUREMENT = "en_US.UTF-8";
-  #   LC_MONETARY = "en_US.UTF-8";
-  #   LC_NAME = "en_US.UTF-8";
-  #   LC_NUMERIC = "en_US.UTF-8";
-  #   LC_PAPER = "en_US.UTF-8";
-  #   LC_TELEPHONE = "en_US.UTF-8";
-  #   LC_TIME = "en_US.UTF-8";
-  # };
-
-  # # Configure keymap in X11
-  # services.xserver = {
-  #   layout = "us";
-  #   xkbVariant = "";
-  # };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.justin = {
-  #   isNormalUser = true;
-  #   description = "justin";
-  #   extraGroups = [ "networkmanager" "wheel" ];
-  #   packages = with pkgs; [ gcc ];
-  # };
-
-  # Enable automatic login for the user.
   services.getty.autologinUser = "justin";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     helix
-    
-        #  wget
   ];
 
   programs.nix-ld.enable = true;
@@ -76,24 +29,12 @@
   # List services that you want to enable:
   programs.git.enable = true;
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
   frgd = {
     archetypes.server.enable = true;
   };
   services.vscode-server.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
 }
