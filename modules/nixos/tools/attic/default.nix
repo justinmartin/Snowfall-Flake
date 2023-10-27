2f9586ab148390082e39d5efaecf067c22684752
Justin Martin <jus10mar10@gmail.com>
2023-10-26 20:04:02 -0500

    disable attic

--- modules/nixos/tools/attic/default.nix
+++ modules/nixos/tools/attic/default.nix
@@ -2,17 +2,9 @@
 
 with lib;
 with lib.frgd;
-let
-  cfg = config.frgd.tools.attic;
-in
-{
-  options.frgd.tools.attic = {
-    enable = mkEnableOption "Attic";
-  };
+let cfg = config.frgd.tools.attic;
+in {
+  options.frgd.tools.attic = { enable = mkEnableOption "Attic"; };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      attic
-    ];
-  };
+  config = mkIf cfg.enable { environment.systemPackages = with pkgs; [ ]; };
 }
