c8fd7b079566627f46792eac0d91ad407023c017
Justin Martin <jus10mar10@gmail.com>
2023-11-02 21:18:05 -0500

    reformat

--- modules/nixos/apps/_1password/default.nix
+++ modules/nixos/apps/_1password/default.nix
@@ -2,10 +2,8 @@
 
 with lib;
 with lib.frgd;
-let
-  cfg = config.frgd.apps._1password;
-in
-{
+let cfg = config.frgd.apps._1password;
+in {
   options.frgd.apps._1password = with types; {
     enable = mkBoolOpt false "Whether or not to enable 1password.";
   };
@@ -15,7 +13,6 @@ in
       _1password = enabled;
       _1password-gui = {
         enable = true;
-
         polkitPolicyOwners = [ config.frgd.user.name ];
       };
     };
