78ebf038cea5c1a223d1121d49a23035c0a20a0a
Justin Martin <jus10mar10@gmail.com>
2023-11-13 13:22:57 -0600

    reformat

--- modules/nixos/tools/qmk/default.nix
+++ modules/nixos/tools/qmk/default.nix
@@ -3,19 +3,16 @@
 with lib;
 with lib.frgd;
 let cfg = config.frgd.tools.qmk;
-in
-{
+in {
   options.frgd.tools.qmk = with types; {
     enable = mkBoolOpt false "Whether or not to enable QMK";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      qmk
-    ];
+    environment.systemPackages = with pkgs; [ qmk vial ];
 
-    services.udev.packages = with pkgs; [
-      qmk-udev-rules
-    ];
+    hardware.keyboard.qmk.enable = true;
+
+    services.udev.packages = with pkgs; [ qmk-udev-rules ];
   };
 }
