3db165eba427a7d7efb6ef418fde79b3cedf8ab2
Justin Martin <jus10mar10@gmail.com>
2023-11-13 13:22:15 -0600

    Allow insecure electron for Logseq

--- modules/home/apps/logseq/default.nix
+++ modules/home/apps/logseq/default.nix
@@ -3,14 +3,13 @@
 with lib;
 with lib.frgd;
 let cfg = config.frgd.apps.logseq;
-in
-{
+in {
   options.frgd.apps.logseq = with types; {
     enable = mkBoolOpt false "Whether or not to enable logseq.";
   };
 
-  config =
-    mkIf cfg.enable {
-      home.packages = with pkgs; [ logseq ];
-    };
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ logseq ];
+    nixpkgs.config.permittedInsecurePackages = [ "electron-24.8.6" ];
+  };
 }
