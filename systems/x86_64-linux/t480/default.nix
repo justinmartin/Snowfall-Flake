11a0bf1b01420897c5d5c2e2c6751f779daee8a5
Justin Martin <jus10mar10@gmail.com>
2023-12-15 20:24:13 -0600

    add t480 config

---
+++ systems/x86_64-linux/t480/default.nix
@@ -0,0 +1,25 @@
+{ lib, ... }:
+with lib;
+with lib.frgd; {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware.nix
+  ];
+
+  networking.hostName = "t480"; # Define your hostname.
+
+  frgd = {
+    archetypes = {
+
+    };
+
+    security = {
+      agenix = {
+        enable = true;
+        taskwarrior = enabled;
+      };
+    };
+  };
+
+  system.stateVersion = "23.11";
+}
---
+++ systems/x86_64-linux/t480/disko.nix
@@ -0,0 +1,62 @@
+{
+  disko.devices = {
+    disk = {
+      x = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "550M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        # mode = "single";
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+        };
+        mountpoint = "/";
+        postCreateHook = "zfs snapshot zroot@blank";
+
+        datasets = {
+          zfs_fs = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          nix = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          home_jusitn = {
+            type = "zfs_fs";
+            mountpoint = "/home/justin";
+            options."com.sun:auto-snapshot" = "true";
+          };
+        };
+      };
+    };
+  };
+}
+
