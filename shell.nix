a1e79e2bf28bd016ad1a7907a19bed32f7410774
Justin Martin <jus10mar10@gmail.com>
2023-12-19 21:43:53 -0600

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,9 @@
+# Shell for bootstrapping flake-enabled nix and home-manager
+# You can enter it through 'nix develop' or (legacy) 'nix-shell'
+
+{ pkgs ? (import <nixpkgs>) { } }: {
+  default = pkgs.mkShell {
+    NIX_CONFIG = "experimental-features = nix-command flakes";
+    nativeBuildInputs = with pkgs; [ nix neovim gcc nixfmt ripgrep alejandra ];
+  };
+}
