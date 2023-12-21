dd2b6b411143b3a29bfa6e0cefbb3405990b18c1
Justin Martin <jus10mar10@gmail.com>
2023-12-20 21:58:21 -0600

    add docker module

---
+++ modules/nixos/virtualization/docker/default.nix
@@ -0,0 +1,18 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.frgd;
+let cfg = config.frgd.virtualization.docker;
+in {
+  options.frgd.virtualization.docker = with types; {
+    enable = mkBoolOpt false "Whether or not to enable docker";
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+    virtualisation.docker.rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+}
