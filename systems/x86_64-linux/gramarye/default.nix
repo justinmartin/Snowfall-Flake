80e99aba88b19c683b37bece7ec707a13b733e7d
Justin Martin <jus10mar10@gmail.com>
2023-12-23 21:58:38 -0600

    add inititial gramarye files

---
+++ systems/x86_64-linux/gramarye/default.nix
@@ -0,0 +1,33 @@
+{ lib, ... }:
+with lib;
+with lib.frgd; {
+  imports = [ ./hardware.nix ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+
+  networking = {
+    hostName = "gramarye";
+    domain = "frigidplatyp.us";
+    networkmanager.enable = true;
+  };
+
+  frgd = {
+    archetypes.server.enable = true;
+    services = { wireguard-server = { enable = true; }; };
+    security = {
+      agenix = {
+        enable = true;
+        taskwarrior = enabled;
+      };
+      sops = {
+        enable = true;
+        # taskwarrior = enabled;
+        vultr_api_key = enabled;
+        matrix_registration_shared_secret = enabled;
+        wireguard_server_key = enabled;
+      };
+    };
+  };
+}
