f3a5d9ea9e1daf9ac194c1fdfaf4cca16cbf3500
Justin Martin <jus10mar10@gmail.com>
2023-12-26 17:17:48 -0600

    add static ip

--- systems/x86_64-linux/gitnix/default.nix
+++ systems/x86_64-linux/gitnix/default.nix
@@ -3,6 +3,15 @@ with lib;
 with lib.frgd; {
   imports = [ ./hardware.nix ];
 
+  networking = {
+    networkmanager.enable = true;
+    interfaces.ens18.ipv4.addresses = [{
+      address = "192.168.0.31";
+      prefixLength = 24;
+    }];
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "192.168.0.1" ];
+  };
   frgd = {
     archetypes.server.enable = true;
     services = { forgejo = enabled; };
