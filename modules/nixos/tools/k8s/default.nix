2c1420f5b8ffe6f14e7b0346171a4b2b65cb3412
Justin Martin <jus10mar10@gmail.com>
2024-01-10 16:47:41 -0600

    cleanup

---
+++ modules/nixos/tools/k8s/default.nix
@@ -0,0 +1,31 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.frgd;
+let cfg = config.frgd.tools.k8s;
+in
+{
+  options.frgd.tools.k8s = with types; {
+    enable =
+      mkBoolOpt false "Whether or not to enable common Kubernetes utilities.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.zsh.shellAliases = {
+      k = "kubecolor";
+      kubectl = "kubecolor";
+      kc = "kubectx";
+      kn = "kubens";
+      ks = "kubeseal";
+    };
+
+    environment.systemPackages = with pkgs; [
+      kubectl
+      kubectx
+      kubeseal
+      kubecolor
+      kubernetes-helm
+      helmfile
+    ];
+  };
+}
