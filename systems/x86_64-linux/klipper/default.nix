44cff6fdb22a75949f0891f53bc3eaae9408d102
Justin Martin <jus10mar10@gmail.com>
2024-01-09 22:34:45 -0600

    update ip and boot options

--- systems/x86_64-linux/klipper/default.nix
+++ systems/x86_64-linux/klipper/default.nix
@@ -7,7 +7,7 @@ with lib.frgd; {
   networking = {
     networkmanager.enable = true;
     interfaces.enp4s0.ipv4.addresses = [{
-      address = "192.168.0.35";
+      address = "192.168.0.8";
       prefixLength = 24;
     }];
     defaultGateway = "192.168.0.1";
--- systems/x86_64-linux/klipper/hardware.nix
+++ systems/x86_64-linux/klipper/hardware.nix
@@ -11,7 +11,10 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
 
+  networking.hostId = "34e61655";
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
