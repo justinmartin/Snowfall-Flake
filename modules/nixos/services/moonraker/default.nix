12fb85224d21795b4db235843201b699b10c7f1b
Justin Martin <jus10mar10@gmail.com>
2024-01-10 16:04:54 -0600

    useable klipper conf

--- modules/nixos/services/moonraker/default.nix
+++ modules/nixos/services/moonraker/default.nix
@@ -13,6 +13,7 @@ in {
       user = "root";
       enable = true;
       address = "0.0.0.0";
+      allowSystemControl = true;
       settings = {
         octoprint_compat = { };
         history = { };
@@ -24,6 +25,7 @@ in {
             "*://app.fluidd.xyz"
             "*://my.mainsail.xyz"
             "*://klipper.frgd.us"
+            "*://klipper.fluffy-rooster.ts.net"
           ];
           trusted_clients = [
             "10.0.0.0/8"
@@ -31,6 +33,7 @@ in {
             "169.254.0.0/16"
             "172.16.0.0/12"
             "192.168.0.0/24"
+            "100.85.139.61/32"
             "FE80::/10"
             "::1/128"
           ];
