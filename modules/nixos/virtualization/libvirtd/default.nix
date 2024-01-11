1a5488e448bc87a09a496ff5d2c8e6cfe182731c
Justin Martin <jus10mar10@gmail.com>
2024-01-10 20:50:53 -0600

    update libvirtd

--- modules/nixos/virtualization/libvirtd/default.nix
+++ modules/nixos/virtualization/libvirtd/default.nix
@@ -6,18 +6,23 @@ let cfg = config.frgd.virtualization.libvirtd;
 in {
   options.frgd.virtualization.libvirtd = with types; {
     enable = mkBoolOpt false "Whether or not to enable libvirtd";
+    virt-manager = {
+      enable = mkBoolOpt false "Whether or not to enable Virt-Manager";
+    };
   };
 
   config = mkIf cfg.enable {
     virtualisation.libvirtd = {
       enable = true;
-      qemuOvmf = true;
+      qemu.ovmf = enabled;
     };
-    environment.systemPackages = with pkgs;
-      [
-        # For lsusb
-        usbutils
-      ];
+    environment = mkIf cfg.virt-manager.enable {
+      systemPackages = with pkgs;
+        [
+          # For lsusb
+          virt-manager
+        ];
+    };
     frgd.user.extraGroups = [ "libvirt" ];
   };
 }
