856c106b30b59819e59510adb8a96faeb921791e
Justin Martin <jus10mar10@gmail.com>
2024-02-22 12:05:44 -0600

    add python packages for taskwarrior

--- modules/home/cli-apps/taskwarrior/default.nix
+++ modules/home/cli-apps/taskwarrior/default.nix
@@ -27,26 +27,21 @@ in {
         context.home.read = "project.not:Western";
         context.home.write = "project.not:Western";
         taskd = mkIf cfg.enable (mkMerge [
-
           {
-
             server = "tasks.frgd.us:53589";
             credentials = "frgd/justin/6d45587b-6254-449c-a6c9-6f8d7989dc19";
             trust = "strict";
           }
           (mkIf (pkgs.stdenv.isLinux) {
-
             certificate = "/run/secrets/taskwarrior_public_cert";
             key = "/run/secrets/taskwarrior_private_key";
             ca = "/run/secrets/taskwarrior_ca_cert";
           })
 
           (mkIf (pkgs.stdenv.isDarwin) {
-
             certificate = "~/.task/keys/public.cert";
             key = "~/.task/keys/private.key";
             ca = "~/.task/keys/ca.cert";
-
           })
         ]);
       };
@@ -60,6 +55,8 @@ in {
       pkgs.taskwarrior-tui
       pkgs.tasksh
       pkgs.vit
+      (pkgs.python3.withPackages
+        (python-pkgs: [ python-pkgs.tasklib python-pkgs.bugwarrior ]))
     ] else [
       pkgs.taskwarrior-tui
       pkgs.tasksh
