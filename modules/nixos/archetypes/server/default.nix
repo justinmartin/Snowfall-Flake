2e8bb09f67a43cb473138509fd29332ee59f9724
Justin Martin <jus10mar10@gmail.com>
2024-02-26 14:25:56 -0600

    reformat, remove tmux from nixos.
    Now using in home-manager

--- modules/nixos/archetypes/server/default.nix
+++ modules/nixos/archetypes/server/default.nix
@@ -2,24 +2,19 @@
 
 with lib;
 with lib.frgd;
-let
-  cfg = config.frgd.archetypes.server;
-in
-{
+let cfg = config.frgd.archetypes.server;
+in {
   options.frgd.archetypes.server = with types; {
-    enable =
-      mkBoolOpt false "Whether or not to enable the server archetype.";
+    enable = mkBoolOpt false "Whether or not to enable the server archetype.";
   };
 
   config = mkIf cfg.enable {
     frgd = {
-      suites = {
-        common-slim = enabled;
-      };
+      suites = { common-slim = enabled; };
 
       cli-apps = {
-#        neovim = enabled;
-        tmux = enabled;
+        #        neovim = enabled;
+        # tmux = enabled;
       };
     };
   };
