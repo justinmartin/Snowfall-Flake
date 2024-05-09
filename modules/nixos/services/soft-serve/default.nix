00fe05726b88a22ded7bd01a137fb3917cd991c2
Justin Martin <jus10mar10@gmail.com>
2024-05-08 20:58:11 -0500

    update soft-serve

--- modules/nixos/services/soft-serve/default.nix
+++ modules/nixos/services/soft-serve/default.nix
@@ -13,15 +13,15 @@ in {
       enable = true;
       settings = {
         name = "FRGD Repo";
-        initial_admin_keys = [''
-          ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDM6+N204Jtp7DCXE8vS/8ZZsWPWSIkntc4Q6cvOT7Gkj+kI/duJ/UHdpboj/514h7DerF6Y7H5MOFBvltqmZvEY42mK7PRGE/5i5Jnf7rIHkI5a7ju4hJCGvH8I/7cqPuYu/TTe8OuZCDYDb00WMDYzujW/rAHyffBAbfM0609lRSIf
-          BlEY5ijGTksZMWEKzgJpbFXG4XZZju/mLX8O0Hu/PxgDEct7KGpTHYX+pJI9UUL/UQg6KnHxhTBfpdVaX73akyauzL/aXpnFkjHQAixEkupM++pddp16+2k3EtLwHSxspwx//IBTaPIe9pujYPOO5n91DvDeUKuCoJ1jvGy/4iq6Ca7JXLwArbRPrSqET3G1zNPsDY4aekXCwTZ2G/K2Y
-          0VnGPFu3+hfea0aBHEGao/GmAHvkRF+SQI2GPG1B5ZR94DW2PI1mtZigUonrNuyu/X5ax/6uoxEJvV8zKpUj4qb2U+ODzmiNwmxIhyxBxoCraW5z9J+mPcPhxirHk= justin@gramarye'']; # Set before initial set up on new machine.  Do not use a key that wil be a user key.
+        ssh.public_url = "ssh://git.frgd.us:23231";
+        initial_admin_keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLUmRA2xE2ff5Z8pW/I/vKVgWnFKV71vArcdYD+OfIH justin@gramarye"
+        ]; # Set before initial set up on new machine.  Do not use a key that wil be a user key.
       };
     };
     networking.firewall = {
-      allowedTCPPorts = [ 23231 23232 23233 ];
-      allowedUDPPorts = [ 23231 23232 23233 ];
+      allowedTCPPorts = [ 23231 ]; # 23232 23233 ];
+      allowedUDPPorts = [ 23231 ]; # 23232 23233 ];
     };
   };
 }
