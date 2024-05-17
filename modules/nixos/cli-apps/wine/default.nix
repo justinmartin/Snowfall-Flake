ee6290daac89d9de31dfa64973d69d877f19b4e4
Justin Martin <jus10mar10@gmail.com>
2024-05-17 11:06:41 -0500

    update winePackages

--- modules/nixos/cli-apps/wine/default.nix
+++ modules/nixos/cli-apps/wine/default.nix
@@ -1,18 +1,26 @@
-{ options, config, lib, pkgs, ... }:
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.cli-apps.wine;
-in {
+let
+  cfg = config.frgd.cli-apps.wine;
+in
+{
   options.frgd.cli-apps.wine = with types; {
     enable = mkBoolOpt false "Whether or not to enable Wine.";
   };
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      winePackages.unstable
+      winePackages
       winetricks
-      wine64Packages.unstable
+      wine64Packages
     ];
   };
 }
