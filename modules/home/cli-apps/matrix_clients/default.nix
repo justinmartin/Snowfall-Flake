c0ccf97f1c824f6bf280bfc927684490f01f1785
Justin Martin <jus10mar10@gmail.com>
2024-05-17 11:00:08 -0500

    add cli matrix apps

---
+++ modules/home/cli-apps/matrix_clients/default.nix
@@ -0,0 +1,25 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+with lib.frgd;
+let
+  cfg = config.frgd.cli-apps.matrix_clients;
+in
+{
+  options.frgd.cli-apps.matrix_clients = with types; {
+    enable = mkBoolOpt false "matrix_clients";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      iamb
+      gomuks
+    ];
+  };
+}
