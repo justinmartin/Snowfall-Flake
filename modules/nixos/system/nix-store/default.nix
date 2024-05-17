e4ac8243034117c6bd55c4c91ca5f6de21deefaa
Justin Martin <jus10mar10@gmail.com>
2024-05-17 11:05:55 -0500

    add local nix substituter

---
+++ modules/nixos/system/nix-store/default.nix
@@ -0,0 +1,24 @@
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+with lib;
+with lib.frgd;
+let
+  cfg = config.frgd.system.nix-store;
+in
+{
+  options.frgd.system.nix-store = with types; {
+    enable = mkBoolOpt false "Whether or not to enable to cache on tsnix.";
+  };
+
+  config = mkIf cfg.enable {
+    frgd.nix.extra-substituters = {
+      "http://tsnix:5000".key = "frgd:Iurj/2/DemnBejCyQWFra3+FKWIEEEnixqe9kwMvbmY=";
+    };
+  };
+}
