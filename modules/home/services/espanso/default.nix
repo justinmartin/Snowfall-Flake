7e599ec3233e7a3c35eeb2c1be868936ad286e53
Justin Martin <jus10mar10@gmail.com>
2024-05-17 11:08:04 -0500

    reformat

--- modules/home/services/espanso/default.nix
+++ modules/home/services/espanso/default.nix
@@ -1,23 +1,32 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.services.espanso;
-in {
+let
+  cfg = config.frgd.services.espanso;
+in
+{
   options.frgd.services.espanso = with types; {
     enable = mkBoolOpt false "espanso";
-    western_snippets = { enable = mkBoolOpt false "Enable Western snippets"; };
-
+    western_snippets = {
+      enable = mkBoolOpt false "Enable Western snippets";
+    };
   };
 
   config = mkIf cfg.enable {
     services.espanso = {
       enable = mkIf pkgs.stdenv.isLinux true;
+      package = mkIf pkgs.stdenv.isLinux pkgs.espanso-wayland;
       configs = {
         default = {
           show_notifications = true;
           search_trigger = "off";
-          search_shortcut = "CTRL+SHIFT+SPACE";
+          #search_shortcut = "CTRL+SHIFT+SPACE";
           clipboard_threshold = 100;
         };
         foot = {
@@ -30,8 +39,8 @@ in {
     # home = { packages = with pkgs; [ espanso ]; };
     # xdg.configFile."espanso/config/default.yml".source = ./config.yml;
     xdg.configFile."espanso/match/base.yml".source = ./base.yml;
-    xdg.configFile."espanso/match/western.yml" =
-      mkIf cfg.western_snippets.enable { source = ./western.yml; };
-
+    xdg.configFile."espanso/match/western.yml" = mkIf cfg.western_snippets.enable {
+      source = ./western.yml;
+    };
   };
 }
