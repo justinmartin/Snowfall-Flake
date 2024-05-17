d5ac70d62b2800fb0d6adc30e258f4dc26803c50
Justin Martin <jus10mar10@gmail.com>
2024-05-17 11:06:21 -0500

    initial print server
    still need remote administration set up

--- modules/nixos/services/printing/default.nix
+++ modules/nixos/services/printing/default.nix
@@ -8,10 +8,17 @@ in {
     enable = mkBoolOpt false "Whether or not to configure printing support.";
     gui = mkBoolOpt false
       "Whether or not to enable a gui to configure printing support.";
+    openFirewall =
+      mkBoolOpt false "Whether or not to open firewall for printing";
   };
 
   config = mkIf cfg.enable {
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
+      openFirewall = (mkIf cfg.openFirewall) true;
+      listenAddresses = [ "*:631" ];
+    };
+
     environment.systemPackages = (mkIf cfg.gui) [ pkgs.system-config-printer ];
   };
 }
