c0f507e4e54d44bcb14742bf8d6bbf3dc403e8d9
Justin Martin <jus10mar10@gmail.com>
2024-06-04 10:06:01 -0500

    reformat

--- modules/nixos/services/espanso/default.nix
+++ modules/nixos/services/espanso/default.nix
@@ -1,20 +1,32 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.services.espanso;
-in {
+let
+  cfg = config.frgd.services.espanso;
+in
+{
   options.frgd.services.espanso = with types; {
     enable = mkBoolOpt false "espanso";
-    western_snippets = { enable = mkBoolOpt false "Enable Western snippets"; };
+    western_snippets = {
+      enable = mkBoolOpt false "Enable Western snippets";
+    };
   };
 
   config = mkIf cfg.enable {
-    services.espanso = { enable = true; };
+    services.espanso = {
+      enable = true;
+    };
     # home = { packages = with pkgs; [ espanso ]; };
     frgd.home.configFile."espanso/config/default.yml".source = ./config.yml;
     frgd.home.configFile."espanso/match/base.yml".source = ./base.yml;
-    frgd.home.configFile."espanso/match/western.yml" =
-      mkIf cfg.western_snippets.enable { source = ./western.yml; };
+    frgd.home.configFile."espanso/match/western.yml" = mkIf cfg.western_snippets.enable {
+      source = ./western.yml;
+    };
   };
 }
