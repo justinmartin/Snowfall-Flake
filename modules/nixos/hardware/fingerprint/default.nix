0098b5274fb6540ef5716f83292e3e8a5d99ad34
Justin Martin <jus10mar10@gmail.com>
2024-07-09 14:31:38 -0500

    finger is semi working for t480

--- modules/nixos/hardware/fingerprint/default.nix
+++ modules/nixos/hardware/fingerprint/default.nix
@@ -1,13 +1,32 @@
-{ options, config, pkgs, lib, ... }:
+{
+  # options,
+  config,
+  # pkgs,
+  lib,
+  inputs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.hardware.fingerprint;
+let
+  cfg = config.frgd.hardware.fingerprint;
 in
 {
   options.frgd.hardware.fingerprint = with types; {
     enable = mkBoolOpt false "Whether or not to enable fingerprint support.";
+    t480 = mkBoolOpt false "Whether or not to enable t480 support.";
   };
 
-  config = mkIf cfg.enable { services.fprintd.enable = true; };
+  config = mkIf cfg.enable {
+    services.fprintd = {
+      enable = true;
+      tod = mkIf cfg.t480 {
+        enable = true;
+        driver = inputs.nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
+          calib-data-file = ./calib-data.bin;
+        };
+      };
+    };
+  };
 }
