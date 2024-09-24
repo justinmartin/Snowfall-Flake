423f27f65cc5204c43047686f61628aa2c1792e8
Justin Martin <jus10mar10@gmail.com>
2024-09-24 07:39:50 -0500

    reformat

--- modules/home/tools/charms/default.nix
+++ modules/home/tools/charms/default.nix
@@ -1,15 +1,28 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.tools.charms;
-in {
+let
+  cfg = config.frgd.tools.charms;
+in
+{
   options.frgd.tools.charms = with types; {
     enable = mkBoolOpt false "Whether or not to enable charms from charm.sh.";
   };
 
   config = mkIf cfg.enable {
 
-    home.packages = with pkgs; [ gum glow pop skate soft-serve wishlist ];
+    home.packages = with pkgs; [
+      gum
+      glow
+      pop
+      skate
+      wishlist
+    ];
   };
 }
