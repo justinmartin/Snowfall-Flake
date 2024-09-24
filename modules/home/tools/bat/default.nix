66b9028fe73d696eadfad4bfbf240878b4485c75
Justin Martin <jus10mar10@gmail.com>
2024-09-24 07:39:39 -0500

    add extra packages to bat

--- modules/home/tools/bat/default.nix
+++ modules/home/tools/bat/default.nix
@@ -1,12 +1,36 @@
-{ options, config, lib, pkgs, ... }:
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.tools.bat;
-in {
+let
+  cfg = config.frgd.tools.bat;
+in
+{
   options.frgd.tools.bat = with types; {
     enable = mkBoolOpt false "Whether or not to enable bat.";
   };
 
-  config = mkIf cfg.enable { programs.bat = { enable = true; }; };
+  config = mkIf cfg.enable {
+    programs.bat = {
+      enable = true;
+      config = {
+        theme = "gruvbox-dark";
+      };
+      extraPackages = with pkgs.bat-extras; [
+        batdiff
+        batman
+        batgrep
+        batwatch
+        prettybat
+        batpipe
+      ];
+
+    };
+  };
 }
