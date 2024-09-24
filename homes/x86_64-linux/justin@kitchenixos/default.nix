b49bb952c1c9cbd2517c5daf1d6669bfc1280555
Justin Martin <jus10mar10@gmail.com>
2024-09-24 07:38:48 -0500

    add atuin and espanso

--- homes/x86_64-linux/justin@kitchenixos/default.nix
+++ homes/x86_64-linux/justin@kitchenixos/default.nix
@@ -1,10 +1,17 @@
 { lib, ... }:
 with lib;
-with lib.frgd; {
+with lib.frgd;
+{
   frgd = {
     user = {
       enable = true;
       name = "justin";
+    };
+    cli-apps = {
+      atuin = enabled;
+    };
+    services = {
+      espanso = enabled;
     };
   };
 }
