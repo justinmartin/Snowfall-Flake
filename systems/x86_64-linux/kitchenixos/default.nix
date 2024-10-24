759471ac464968523ced601f2cfe6c890a03ddd7
Justin Martin <jus10mar10@gmail.com>
2024-10-24 07:47:25 -0500

    reenable sound

--- systems/x86_64-linux/kitchenixos/default.nix
+++ systems/x86_64-linux/kitchenixos/default.nix
@@ -15,6 +15,16 @@ with lib.frgd;
   hardware.bluetooth.powerOnBoot = true;
   services.flatpak.enable = true;
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     wezterm
     alacritty
