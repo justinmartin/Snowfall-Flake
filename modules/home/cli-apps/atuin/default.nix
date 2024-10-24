dd541a1c7d3c12c456aa6a9218548117e8f50d4f
Justin Martin <jus10mar10@gmail.com>
2024-10-24 07:45:41 -0500

    add a few config options

--- modules/home/cli-apps/atuin/default.nix
+++ modules/home/cli-apps/atuin/default.nix
@@ -5,8 +5,22 @@ let
   inherit (lib.frgd) enabled;
 
   cfg = config.frgd.cli-apps.atuin;
-in {
-  options.frgd.cli-apps.atuin = { enable = mkEnableOption "atuin"; };
+in
+{
+  options.frgd.cli-apps.atuin = {
+    enable = mkEnableOption "atuin";
+  };
 
-  config = mkIf cfg.enable { programs.atuin = { enable = true; }; };
+  config = mkIf cfg.enable {
+    programs.atuin = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = {
+        auto_sync = true;
+        sync_frequency = "5m";
+        enter_accept = true;
+        # search_mode = "prefix";
+      };
+    };
+  };
 }
