712961d8eee78f1f21bf774f763c19272fd21b9e
Justin Martin <jus10mar10@gmail.com>
2024-10-24 07:45:25 -0500

    set up alacritty for VIT
    alacritty automatically starts vit for task management

---
+++ modules/home/apps/alacritty/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+with lib.frgd;
+let
+  cfg = config.frgd.apps.alacritty;
+in
+{
+  options.frgd.apps.alacritty = with types; {
+    enable = mkBoolOpt false "Whether or not to enable alacritty.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        font.size = 18;
+        decorations = "buttonless";
+      };
+
+    };
+
+  };
+}
