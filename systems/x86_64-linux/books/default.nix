37016a77f913fcd1efed8450627e23cb68b55606
Justin Martin <jus10mar10@gmail.com>
2024-11-07 21:09:50 -0600

    initial books config

--- systems/x86_64-linux/books/default.nix
+++ systems/x86_64-linux/books/default.nix
@@ -19,12 +19,33 @@ with lib.frgd;
     defaultGateway = "10.10.4.1";
     nameservers = [ "10.10.4.1" ];
   };
+  # services.caddy = {
+  #   enable = true;
+  #   virtualHosts = {
+  #     "books.fluffy-rooster.ts.net" = {
+  #       extraConfig = ''
+  #         reverse_proxy http://127.0.0.1:8083
+  #         encode gzip
+  #       '';
+  #     };
+  #     "books.frgd.us" = {
+  #       useACMEHost = "books.frgd.us";
+  #       extraConfig = ''
+  #         reverse_proxy http://127.0.0.1:8083
+  #         encode gzip
+  #       '';
+  #     };
+  #   };
+  # };
+
   frgd = {
+    nix = enabled;
     archetypes.server = enabled;
     security.sops = enabled;
     services = {
       syncthing = enabled;
       tailscale = enabled;
+      calibre-web = enabled;
     };
     system.boot.efi = true;
   };
--- systems/x86_64-linux/books/disko.nix
+++ systems/x86_64-linux/books/disko.nix
@@ -13,7 +13,7 @@
               content = {
                 type = "filesystem";
                 format = "vfat";
-                mountpoint = "/boot";
+                mountpoint = "/boot/efi";
                 mountOptions = [ "umask=0077" ];
               };
             };
