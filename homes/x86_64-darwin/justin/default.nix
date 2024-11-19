95fbda40527277b137d69f3107a3c19d8d9c6c5e
Justin Martin <jus10mar10@gmail.com>
2024-11-19 10:40:20 -0600

    add default x86_64 darwin home-manager config

---
+++ homes/x86_64-darwin/justin/default.nix
@@ -0,0 +1,33 @@
+{ lib, ... }:
+with lib;
+with lib.frgd;
+{
+  frgd = {
+    services = {
+      espanso = {
+        enable = true;
+      };
+    };
+
+    cli-apps = {
+      neovim = enabled;
+      home-manager = enabled;
+      tmux = enabled;
+      system-monitors = enabled;
+      taskwarrior = enabled;
+      #zellij = enabled;
+      #fish = enabled;
+    };
+
+    tools = {
+      git = enabled;
+      direnv = enabled;
+      bat = enabled;
+      misc = enabled;
+      charms = enabled;
+    };
+  };
+  programs.taskwarrior.dataLocation ="/Users/justin/Library/Mobile Documents/iCloud~com~mav~taskchamp/Documents/task";
+  home.stateVersion = "24.05";
+
+}
