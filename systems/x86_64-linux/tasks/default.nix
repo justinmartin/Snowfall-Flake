ece03cd428d2d945b50efc491ca86486113220bb
Justin Martin <jus10mar10@gmail.com>
2024-11-20 12:13:59 -0600

    reformat, remove caddy

--- systems/x86_64-linux/tasks/default.nix
+++ systems/x86_64-linux/tasks/default.nix
@@ -19,23 +19,14 @@ with lib.frgd;
     defaultGateway = "10.10.4.1";
     nameservers = [ "10.10.4.1" ];
   };
-  services.caddy = {
-    enable = true;
-    virtualHosts = {
-      "tasks.fluffy-rooster.ts.net" = {
-        extraConfig = ''
-          reverse_proxy http://127.0.0.1:8084
-          encode gzip
-        '';
-      };
-    };
-  };
 
   frgd = {
     archetypes.vm = enabled;
     nix = enabled;
-    services.taskchampion = {
-      enable = true;
+    services = {
+      taskchampion = {
+        enable = true;
+      };
     };
     virtualization.docker = enabled;
   };
