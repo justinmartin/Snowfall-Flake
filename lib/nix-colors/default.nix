ab12dc9ab5f09f9c3b09eb6f65fe4b84ee235f52
Justin Martin <jus10mar10@gmail.com>
2024-12-15 21:45:57 -0600

    revert to gruvbox, reformat

--- lib/nix-colors/default.nix
+++ lib/nix-colors/default.nix
@@ -1,3 +1,13 @@
-{ inputs, snowfall-inputs, lib, ... }:
-let inherit (inputs) nix-colors;
-in { colorScheme = nix-colors.colorSchemes.gruvbox-dark-medium; }
+{
+  inputs,
+  snowfall-inputs,
+  lib,
+  ...
+}:
+let
+  inherit (inputs) nix-colors;
+in
+{
+  colorScheme = nix-colors.colorSchemes.gruvbox-dark-medium;
+  # colorScheme = nix-colors.colorSchemes.kanagawa;
+}
